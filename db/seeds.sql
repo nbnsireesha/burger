8c9eca2bad28d34f244fc80a62b6965eeea9c64e
nbnsireesha <nbnsireesha@gmail.com>
2018-01-07 20:36:31 -0600

    DID IT

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('Cheez Burger',false);
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('Big Mac',false);
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('Bacon Burger',true);
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('Le Pigeon Burger', false );
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('Whiskey King Burger', false);
+
+
---
+++ models/burger.js
@@ -0,0 +1,29 @@
+//importing orm to implement functions that interact with the database
+var orm = require("../config/orm.js");
+
+//creating a burger object
+var burger = {
+	//select all burger table entries
+	selectAll: function(cb){
+		orm.selectAll('burgers', function(res) {
+			cb(res);
+		});
+	},
+	//
+	insertOne: function(cols, vals, cb){
+		orm.insertOne('burgers', cols, vals, function(res){
+			cb(res);
+		});
+
+	},
+	//
+	updateOne: function(objColVals, condition, cb){
+		orm.updateOne('burgers', objColVals, condition, function(res){
+			cb(res);
+		});
+
+	}
+};
+module.exports = burger;
+
+
